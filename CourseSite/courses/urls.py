16a65dcb1cf07305ff0863b9bbbdabd6399a3bb6
gautam <gautimeena456@gmail.com>
2018-04-02 02:35:28 +0530

    implement Authentication part

--- CourseSite/courses/urls.py
+++ CourseSite/courses/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
 	  url(r'^$', views.index, name = 'index'),
+	  url(r'^register/',views.UserFormView.as_view(), name = 'register'),
+	  url(r'^logout',views.logout_user, name = 'logout'),
+	  url(r'^login/',views.login_user,name = 'login'),
 	  url(r'^detail/', views.detail, name = 'detail'),
 	  url(r'^add_course/', views.add_course, name = 'add_course'),
 	  url(r'^add_review/', views.add_reviews, name = 'add_reviews'),
--- CourseSite/courses/views.py
+++ CourseSite/courses/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Course, Reviews
 
+
+
+from django.views import generic
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
+from django.contrib.auth import logout
+
+from .models import Course, Reviews
 
 def index(request):
 	all_courses = Course.objects.all()
@@ -73,6 +83,72 @@ def profdetails(request):
 	search_str = request.POST['p']
 	field = Reviews.objects.filter(professor = search_str)
 	return render (request, 'courses/profdetails.html', {'field': field})
+
+
+
+class UserFormView(View):
+	form_class = UserForm
+	template_name = 'courses/registration_form.html'
+
+	def get(self, request):
+		form = self.form_class(None)
+		return render(request, self.template_name, {'form' : form})
+
+
+	def post(self, request):
+		form = self.form_class(request.POST)
+
+		if form.is_valid():
+
+			user = form.save(commit = False)
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			user.set_password(password)
+			user.save()
+
+
+			#returns User objecyts if credentials are correct
+
+			user = authenticate(username = username, password = password)
+			
+			if user is not None:
+
+				if user.is_active:
+					login(request,user)
+					return render(request, 'courses/index.html')
+
+		return render(request, self.template_name, {'form': form})
+
+
+
+def login_user(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+
+		user = authenticate(username = username, password = password)
+
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return render(request, 'courses/index.html')
+
+			else:
+				return render(request, 'courses/login.html', {'error_message':'Your account has been disabled'})
+		
+		else:
+			return render(request,'courses/login.html',{'error_message':'invalid login'})
+	
+	return render(request, 'courses/login.html')
+
+
+
+def logout_user(request):
+
+	logout(request)
+	#form = UserForm(request.POST or None)
+	return render(request, 'courses/login.html')
+
 
 
 
